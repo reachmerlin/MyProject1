818d617ebc7bd1c6be113467c5ff43a1bd9d17da
Merlin Raja E <Merlin@savantit.com>
2011-05-06 10:57:16 +0530

    Exercise 1-4

---
+++ exc1.py
@@ -0,0 +1,7 @@
+print "Hello!"
+print "My Name Is Merlin." 
+print "Good Luck"
+print 'Yes! i will come.'
+print "I'd like to come 'yes'."
+print 'testing "python" exc'
+
---
+++ exc3.py
@@ -0,0 +1,13 @@
+#print "I will now count chickens:"
+print "Hens", 25+30/6
+print "roosters", 25-100 * 3%4
+print "now i will count the eggs:"
+#print 3+4+1-5+3.0%4-1/4.0+6
+print "is it true that 3+2<5-7?"
+print 3+2<5-7 # should print false
+print "what is 3+2?:", 3+2
+print "oh, thats why it's false"
+print "is it greater 5>-2?", 5>-2
+print "is it greater or equals 5>=5?", 5>=5
+print "is it less or equal 5<=4?", 5<=4
+
