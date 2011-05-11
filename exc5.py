50741f853f6d15466882032a3c61e7ae1700c702
Merlin Raja E <Merlin@savantit.com>
2011-05-11 16:24:08 +0530

    My learning

--- exc5.py
+++ exc5.py
@@ -1,17 +1,17 @@
-my_name = 'Merlin Raja E.'
-my_age = 25 # not a lie
-my_height = 165 # cm
-my_weight = 72 # kg
-my_eyes = "Brown"
-my_teeth = "white"
-my_hair = 'Black'
+name = 'Merlin Raja Essaya.'
+age = 25 # not a lie
+height = 165 # cm
+weight = 72 # kg
+eyes = "Brown"
+teeth = "white"
+hair = 'Black'
 
-print "Let's talk about %s." % my_name
-print "He's %d 'cm' tall." % my_height
-print "He's %d 'kg' heavy." %my_weight
+print "Let's talk about %s." % name
+print "He's %d 'cm' tall." % height
+print "He's %d 'kg' heavy." %weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 #this line is tricky, try to get it exactly right
-print "If i add %d, %d, and %d i get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If i add %d, %d, and %d i get %d." % (age, height, weight, age + height + weight)
 
